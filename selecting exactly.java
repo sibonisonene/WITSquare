f270d29363c339b7d347e4fccc16274070e8a0be
Siboniso Nene <sibonisodev@gmail.com>
2024-03-23 17:44:27 +0200

    Update selecting exactly.java

--- selecting exactly.java
+++ selecting exactly.java
@@ -3,8 +3,7 @@ import java.util.Collections;
 
 public class RandomLines {
     public static void main(String[] args) {
-        Random rand = new Random();
-        ArrayList<String> lines = new ArrayList<>();
+        Random rand 
 
         for (int i = 0; i < 100; i++) {
             int choice = rand.nextInt(5);
